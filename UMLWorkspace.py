2435db05c0b76ca04e668e79d0f7231a67c5167a
bdsword <thetruebloodsword@gmail.com>
2014-09-29 02:31:11 +0800

    Unselect other UMLComponents when a component is selected, reformat file to fit pep8 standard.

--- UMLWorkspace.py
+++ UMLWorkspace.py
@@ -37,13 +37,9 @@ class UMLWorkspace(DropArea):
 
     def on_button_release(self, widget, event):
         self.state = State.UMLWorkspace.NORMAL
-        self.select_end_point = (event.x ,event.y)
-        # TODO: Make a real select rather than just show a rectangle
+        self.select_end_point = (event.x, event.y)
         selected_rectangle = ToolSets.make_rectangle(self.select_start_point, self.select_end_point)
         self.foreach(self.update_component_selection, selected_rectangle)
-
-        ##########################################33
-
         self.queue_draw()
 
     def update_component_selection(self, widget, rectangle):
@@ -56,7 +52,7 @@ class UMLWorkspace(DropArea):
 
     def on_mouse_motion(self, widget, event):
         if self.state == State.UMLWorkspace.SELECTING:
-            self.select_end_point = (event.x ,event.y)
+            self.select_end_point = (event.x, event.y)
             self.queue_draw()
 
     def on_drag_box_button_press(self, widget, event):
@@ -66,5 +62,11 @@ class UMLWorkspace(DropArea):
     def on_drag_box_button_release(self, widget, event):
         super(UMLWorkspace, self).on_drag_box_button_release(widget, event)
         widget.set_state(State.UMLComponent.SELECTED)
+        self.foreach(self.unselect_other_components, widget)
         self.queue_draw()
         return True
+
+    def unselect_other_components(self, unchecked_widget, selected_target):
+        if unchecked_widget is not selected_target:
+            unchecked_widget.set_state(State.UMLComponent.NORMAL)
+            unchecked_widget.queue_draw()

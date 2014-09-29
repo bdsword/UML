f35a88228ea47f8eef56df41a55ae6b1b2563828
bdsword <thetruebloodsword@gmail.com>
2014-09-29 10:30:05 +0800

    Assert type when add, insert, remove point in/out line.

--- Line.py
+++ Line.py
@@ -1,14 +1,19 @@
+from Point import Point
 
 
 class Line:
     def __init__(self):
+        super(Line, self).__init__()
         self.points = []
 
     def append_point(self, point):
+        assert(isinstance(point, Point))
         self.points.append(point)
 
     def insert_point(self, index, point):
+        assert(isinstance(point, Point))
         self.points.insert(index, point)
 
     def remove_point(self, point):
+        assert(isinstance(point, Point))
         self.points.remove(point)
